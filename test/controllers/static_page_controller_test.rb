563d018b5b807f0fc41da4a5e8bc833df89a17e1
Uy Binh <uybinh@gmail.com>
2018-05-25 05:26:32 +0700

    Update test

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample Page"
+  end
+
   test "should get home" do
     get static_page_home_path
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_page_help_path
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_page_about_path
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
