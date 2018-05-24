cbc2ba1cb1465958deb732070dd8c3ff888ec3da
Uy Binh <uybinh@gmail.com>
2018-05-24 21:07:05 +0700

    Add about page

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -2,12 +2,17 @@ require 'test_helper'
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_page_home_url
+    get static_page_home_path
     assert_response :success
   end
 
   test "should get help" do
-    get static_page_help_url
+    get static_page_help_path
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_page_about_path
     assert_response :success
   end
 
