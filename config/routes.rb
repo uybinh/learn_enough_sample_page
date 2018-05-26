e44003dc642652ea616ceb1ef7b395e8477b1746
Uy Binh <uybinh@gmail.com>
2018-05-26 20:44:38 +0700

    Edit root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'static_page/home'
   get 'static_page/help'
   get 'static_page/about'
+  get 'static_page/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "application#hello"
+  root "static_page#home"
 
 end
