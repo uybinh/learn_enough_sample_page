595e6e00b06bf288b618f309c1dfc0dc653ffe66
Uy Binh <uybinh@gmail.com>
2018-06-22 10:33:30 +0700

    Use strong parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
   root "static_page#home"
   get "/help", to: 'static_page#help'
   get "/about", to: 'static_page#about'
   get "/contact", to: 'static_page#contact'
+  get '/signup', to: 'users#new'
   resources :users
 end
