e9015a9514d31fe6ab5ed851352ac84b57c34ac9
Uy Binh <uybinh@gmail.com>
2018-06-30 20:22:31 +0700

    Update navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
-
-  def home
-    render html: "Hello, world"
-  end
-
-  def index
-
-  end
-
-
-
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
