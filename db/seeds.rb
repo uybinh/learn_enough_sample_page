60656041fae6778548534282b9d660773954fcfc
Uy Binh <uybinh@gmail.com>
2018-07-02 23:50:23 +0700

    Add activation mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,26 @@ User.create!(name: "Uy Binh",
   email: "binh@gmail.com",
   password: "foobar",
   password_confirmation: "foobar",
-  admin: true)
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now)
 
-  User.create!(name: "Minh Xuan",
+User.create!(name: "Minh Xuan",
   email: "xuan@gmail.com",
   password: "foobar",
   password_confirmation: "foobar",
-  admin: true)
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now)
 
 98.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
     email: email,
     password: password,
-    password_confirmation: password)
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now)
 end
