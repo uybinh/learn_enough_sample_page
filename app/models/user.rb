0a8b30b2076bb04ad4fb26cb2b7687f4217da9dd
Uy Binh <uybinh@gmail.com>
2018-07-04 16:23:17 +0700

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -18,7 +18,7 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-
+  
   def User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -38,20 +38,41 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # Activates an account
   def activate
     update_attributes(activated: true,
                       activated_at: Time.zone.now)
   end
 
+  # send activation email
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
 
+  # set password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attributes(reset_digest: User.digest(self.reset_token),
+                      reset_sent_at: Time.zone.now)
+  end
+
+  # send password rest email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now    
+  end
+
+  # 
+  def password_reset_expired?
+    self.reset_sent_at < 2.hours.ago
+  end
+
   private
+    # converts email to all lower-case
     def downcase_email
       self.email.downcase!
     end
 
+    # create and assigns the activation token and digest
     def create_activation_digest
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
